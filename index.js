a1aaeefed9f62a2e0e57773a158cfead15b6cc60
skliarov <ext-serhii.skliarov@here.com>
2023-07-26 17:09:59 +0200

    Deploy test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.helloWorld = (req, res) => {
-  const message = "<font color='blue'>CloudFunctions build on GCP!</font><br><b>App Version 1.0</b>";
+  const message = "<font color='blue'>CloudFunctions build on GCP!</font><br><b>App Version latest</b>";
   res.status(200).send(message);
 }
