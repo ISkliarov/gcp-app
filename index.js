87fae8c6b00264fcdbcd7f5bec2239fa0985f157
skliarov <ext-serhii.skliarov@here.com>
2023-07-26 17:18:01 +0200

    Deploy test:2

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+//-----------------------------------------------------------------------------
+// NodeJS App for GCP Cloud Functions deployed via GCP Cloud Build Triggers
+// Copyleft (c) by Denis Astahov
+//-----------------------------------------------------------------------------
+
 exports.helloWorld = (req, res) => {
-  const message = "<font color='blue'>CloudFunctions build on GCP!</font><br><b>App Version latest</b>";
+  const message = "<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.1</b>";
   res.status(200).send(message);
-}
+};
